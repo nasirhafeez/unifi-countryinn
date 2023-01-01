01c3f4c82d132971075f41b28900dc00be88f2da
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-01-02 04:43:04 +0500

    initial commit

--- default/connect.php
+++ default/connect.php
@@ -3,29 +3,25 @@
 require 'header.php';
 include 'config.php';
 
+$redirect_url = $_SERVER['REDIRECT_URL'];
+
 $mac = $_SESSION["id"];
 $apmac = $_SESSION["ap"];
 $user_type = $_SESSION["user_type"];
 $last_updated = date("Y-m-d H:i:s");
 
 if ($user_type == "new") {
-  $fname = $_POST['fname'];
-  $lname = $_POST['lname'];
-  $email = $_POST['email'];
 
   mysqli_query($con, "
     CREATE TABLE IF NOT EXISTS `$table_name` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `firstname` varchar(45) NOT NULL,
-    `lastname` varchar(45) NOT NULL,
-    `email` varchar(45) NOT NULL,
     `mac` varchar(45) NOT NULL,
     `last_updated` varchar(45) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY (mac)
     )");
 
-  mysqli_query($con,"INSERT INTO `$table_name` (firstname, lastname, email, mac, last_updated) VALUES ('$fname', '$lname', '$email', '$mac', '$last_updated')");
+  mysqli_query($con,"INSERT INTO `$table_name` (mac, last_updated) VALUES ('$mac', '$last_updated')");
 }
 
 $controlleruser = $_SERVER['CONTROLLER_USER'];
@@ -54,7 +50,7 @@ $auth_result = $unifi_connection->authorize_guest($mac, $duration, null, null, n
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="../assets/styles/bulma.min.css" />
     <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
-    <meta http-equiv="refresh" content="5;url=https://www.google.com" />
+    <meta http-equiv="refresh" content="5;url=<?php echo htmlspecialchars($redirect_url); ?>" />
     <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
     <link rel="stylesheet" href="../assets/styles/style.css" />
--- default/index.php
+++ default/index.php
@@ -3,22 +3,35 @@
 require 'header.php';
 include 'config.php';
 
-$_SESSION["id"] = $_GET['id'];
-$_SESSION["ap"] = $_GET['ap'];
-$_SESSION["user_type"] = "new";
+if (isset($_GET['id'])) {
+  $_SESSION["id"] = $_GET['id'];
+  $_SESSION["ap"] = $_GET['ap'];
+  $_SESSION["user_type"] = "new";
+}
 
-# Checking DB to see if user exists or not.
-$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[id]'");
+//# Checking DB to see if user exists or not.
+//$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[id]'");
+//
+//if ($result->num_rows >= 1) {
+//  $row = mysqli_fetch_array($result);
+//
+//  mysqli_close($con);
+//
+//  $_SESSION["user_type"] = "repeat";
+//  header("Location: welcome.php");
+//} else {
+//  mysqli_close($con);
+//}
 
-if ($result->num_rows >= 1) {
-  $row = mysqli_fetch_array($result);
+$code_error = 0;
 
-  mysqli_close($con);
-
-  $_SESSION["user_type"] = "repeat";
-  header("Location: welcome.php");
-} else {
-  mysqli_close($con);
+if (isset($_POST['accept'])) {
+  $access_code = $_SERVER['ACCESS_CODE'];
+  if ($_POST['code'] === $access_code) {
+    header('Location: connect.php');
+  } else {
+    $code_error = 1;
+  }
 }
 
 ?>
@@ -26,76 +39,73 @@ if ($result->num_rows >= 1) {
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <title>
-    <?php echo htmlspecialchars($business_name); ?> WiFi</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  <link rel="stylesheet" href="../assets/styles/bulma.min.css" />
-  <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
-  <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
-  <link rel="stylesheet" href="../assets/styles/style.css" />
+    <meta charset="utf-8">
+    <title>
+      <?php echo htmlspecialchars($business_name); ?> WiFi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" href="../assets/styles/bulma.min.css"/>
+    <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css"/>
+    <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16"/>
+    <link rel="stylesheet" href="../assets/styles/style.css"/>
 </head>
 
 <body>
-  <div class="page">
+<div class="page">
 
     <div class="head">
-      <br>
-      <figure id="logo">
-        <img src="../assets/images/logo.png">
-      </figure>
+        <br>
+        <figure id="logo">
+            <img src="../assets/images/logo.png">
+        </figure>
     </div>
 
     <div class="main">
-      <section class="section">
-        <div class="container">
-          <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">Enter your details</div>
-          <form method="post" action="connect.php">
-            <div class="field">
-                <div class="control has-icons-left">
-                    <input class="input" type="text" id="form_font" name="fname" placeholder="First Name" required>
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-user"></i>
-                    </span>
+        <section class="section">
+            <div class="container">
+                <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">Free Internet Access
                 </div>
-            </div>
+                <div id="login" class="content is-size-6 has-text-centered">
+                    Please review these <a href="policy.php">Terms and Conditions</a>
+                </div>
+                <div id="login" class="content is-size-6 has-text-centered">
+                    Enter your access code and click "I Accept"
+                </div>
+                <br>
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <div class="field">
+                        <div class="control has-icons-left">
+                            <input class="input" type="text" name="code" placeholder="Code"
+                                   required>
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-key"></i>
+                            </span>
+                        </div>
+                    </div>
 
-            <div class="field">
-              <div class="control has-icons-left">
-                  <input class="input" type="text" id="form_font" name="lname" placeholder="Last Name" required>
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                </span>
-              </div>
-            </div>
+                    <?php
+                    if ($code_error == 1) { ?>
+                    <div class="content is-size-6 has-text-centered has-text-danger">
+                        Sorry! The code you entered is incorrect
+                    </div>
+                    <?php
+                    }
+                    ?>
 
-            <div class="field">
-                <div class="control has-icons-left">
-                    <input class="input" type="email" id="form_font" name="email" placeholder="Email" required>
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
+                    <div class="buttons is-centered">
+                        <input class="button is-link" type="submit" name="accept" value="I Accept">
+                    </div>
+                </form>
+                <br>
+                <div class="content is-size-6 has-text-centered">
+                    For Technical Support
+                    <br>
+                    Call 800-650-4373
                 </div>
             </div>
             <br>
-            <div class="columns is-centered is-mobile">
-                <div class="control">
-                    <label class="checkbox">
-                        <input type="checkbox" required>
-                        I agree to the <a href="policy.php">Terms of Use</a>
-                    </label>
-                </div>
-            </div>
-            <br>
-            <div class="buttons is-centered">
-                <button class="button is-link">Connect</button>
-            </div>
-          </form>
-        </div>
-        <br>
-      </section>
+        </section>
     </div>
-  </div>
+</div>
 </body>
 </html>
